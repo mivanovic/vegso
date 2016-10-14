96d54c92bcdcb81339cc0b04a51ea6f038bcd817
Marko <mivanovic@outlook.com>
2016-10-14 10:30:11 +0200

    services images and about us update

--- final/models.py
+++ final/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+CODE_TEXT_LENGTH = 100
+SHORT_TEXT_LENGTH = 500
+LONG_TEXT_LENGTH = 2000
+
+class References(models.Model):
+	title = models.CharField(null=False, max_length=CODE_TEXT_LENGTH)
+	description = models.CharField(max_length=LONG_TEXT_LENGTH)
+	location = models.CharField(max_length=CODE_TEXT_LENGTH)
+	complexity = models.IntegerField(default=1, validators=[MaxValueValidator(100), MinValueValidator(1))
+	# TODO: add multiple choices for labels
+
